5b8146ccdb21341848c659cc3f378bbe1bf3e73b
nickolasj926 <nickolasj926@gmail.com>
2021-09-07 17:09:37 -0500

    Initial commit: Experimenting with if statements. The example is for an organization of people whose age requirement is that you be from 21-30 years old.

---
+++ src/ifStatements.java
@@ -0,0 +1,15 @@
+public class ifStatements {
+
+    public static void main(String[] args) {
+        int boy, girl;
+        boy = 22;
+        girl = 30;
+
+        if(boy >= 21 || girl < 30) {
+            System.out.println("You can join.");
+        }else{
+            System.out.println("You cannot join.");
+        }
+    }
+}
+
